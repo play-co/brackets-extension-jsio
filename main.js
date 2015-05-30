1fc8bea9154044b42e28642e443ee2f8f91f1100
yofreke <sparkyjoe94@aim.com>
2015-05-30 03:52:21 -0700

    initial

---
+++ main.js
@@ -0,0 +1,24 @@
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $ */
+
+define(function (require, exports, module) {
+    "use strict";
+
+    // Brackets modules
+    var EditorManager           = brackets.getModule("editor/EditorManager"),
+        ExtensionUtils          = brackets.getModule("utils/ExtensionUtils"),
+        AppInit                 = brackets.getModule("utils/AppInit");
+
+    /**
+     * Add the bee!
+     */
+    function _addBee() {
+        $("<div class='weeby bee'><div></div></div>").appendTo($('.pane-content')[0]);
+    }
+
+    AppInit.appReady(function () {
+        _addBee();
+    });
+    ExtensionUtils.loadStyleSheet(module, 'jsio.css');
+});
