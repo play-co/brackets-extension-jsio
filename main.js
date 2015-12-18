51aede1a763a64d06b85e384fc08b69dfc68d976
Martin Hunt <martin@gameclosure.com>
2015-12-18 20:55:48 +0700

    add ability to save current file using postmessage

--- main.js
+++ main.js
@@ -7,11 +7,12 @@ define(function (require, exports, module) {
 
     var ExtensionUtils      = brackets.getModule('utils/ExtensionUtils'),
         AppInit             = brackets.getModule('utils/AppInit'),
-        PreferencesManager  = brackets.getModule('preferences/PreferencesManager');
+        PreferencesManager  = brackets.getModule('preferences/PreferencesManager'),
+        CommandManager      = brackets.getModule('command/CommandManager'),
+        Commands      = brackets.getModule('command/Commands');
 
     var preferencesId = 'jsio';
     var _preferences;
-
 
     AppInit.htmlReady(function () {
         _preferences = PreferencesManager.getExtensionPrefs(preferencesId);
@@ -24,4 +25,29 @@ define(function (require, exports, module) {
         $('body').append('<link rel="stylesheet" href="' + ExtensionUtils.getModulePath(module) + 'uitheme.css">');
     });
 
+    window.addEventListener('message', function (event) {
+        var data;
+        try {
+            data = JSON.parse(event.data);
+        } catch (e) {
+            return;
+        }
+
+        if (!data) { return; }
+
+        var action;
+        if (data.action === 'save') {
+            action = CommandManager.execute(Commands.FILE_SAVE);
+        }
+
+        if (action && data._requestId) {
+            action.done(function () {
+                event.source.postMessage(JSON.stringify({
+                    _jsio: true,
+                    target: 'brackets',
+                    responseFor: data._requestId
+                }), '*');
+            });
+        }
+    });
 });
